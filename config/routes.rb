b95fd13fdf9a5d5d0c1b934b9577a1562c2a3657
yukijina <yukijina@gmail.com>
2019-05-24 07:14:03 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :artists, only: [:index, :show] do
+  resources :artists do
     resources :songs, only: [:index, :show]
   end
 
-  resources :artists
+  #resources :artists
   resources :songs
 
   namespace :admin do
